ec3e86d86201a53f9133027f6f1edf09a3a861de
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2018-07-07 19:29:52 +0600

    minimal bulma

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,19 @@
 import '../sass/fonts.scss';
 import '../sass/auth.scss';
 import '../sass/app.scss';
+
+import Vue from 'vue';
+import Buefy from 'buefy';
+
+import App from './App.vue'
+import Navbar from './ui/Navbar'
+
+Vue.config.productionTip = false;
+
+Vue.use(Buefy, {
+    defaultIconPack: 'fas'
+});
+
+new Vue({
+    render: h => h(Navbar)
+}).$mount('#navbar');
---
+++ resources/assets/js/ui/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <nav class="navbar app-navbar">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="/"><i class="fas fa-tty fa-fw fa-2x"></i></a>
+            <a role="button" class="navbar-burger" @click="toggleOpen"><span></span><span></span><span></span></a>
+        </div>
+        <div class="navbar-menu" :class="menuClass">
+
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar",
+        data: function () {
+            return {
+                opened: false,
+            }
+        },
+        computed: {
+            menuClass: function () {
+                return this.opened ? 'is-active' : '';
+            }
+        },
+        methods: {
+            toggleOpen: function () {
+                this.opened = !this.opened
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
