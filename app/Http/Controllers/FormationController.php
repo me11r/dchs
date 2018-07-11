62d7ff9a3c8c0ef7393f79694a9dd01b6a7b3748
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2018-07-11 20:03:49 +0600

    logout

---
+++ app/Http/Controllers/FormationController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gorbunov
+ * Date: 11.07.2018
+ * Time: 19:38
+ */
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class FormationController extends AuthorizedController
+{
+    public function get101(Request $request) {
+
+    }
+
+    public function getAdd101(Request $request, $form_id = 0)
+    {
+
+    }
+
+    public function postAdd101(Request $request, $form_id = 0)
+    {
+
+    }
+
+    public function getViewFull(Request $request, $date = null)
+    {
+
+    }
+
+
+}
