36fecab3833d4138684d774479d3c98ac4cc8965
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2018-07-14 18:21:37 +0600

    preloader added for long timers init

---
+++ app/Dictionary/Street.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gorbunov
+ * Date: 14.07.2018
+ * Time: 18:20
+ */
+
+namespace App\Dictionary;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Street extends Model
+{
+    protected $table = 'streets';
+    protected $guarded = ['id'];
+
+    public function area()
+    {
+        return $this->belongsTo(CityArea::class, 'id', 'city_area_id');
+    }
+}
