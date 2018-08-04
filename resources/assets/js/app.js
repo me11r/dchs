d4d242176aa63eaf8e43e13191b407d08ea59dd0
nbah1990 <uocyxapuk@gmail.com>
2018-08-04 13:47:05 +0600

    101 additional fields

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -12,6 +12,8 @@ import Card from './Card101';
 import {Card112Form} from './views/card112';
 import {HydrantMapList} from './views/hydrant-map';
 
+import Add101Functions from './scripts/add101';
+
 const token = document.head.querySelector('meta[name="csrf-token"]');
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content || '';
@@ -36,4 +38,27 @@ if (document.getElementById(card112FormBlockId)) {
 const hydrantMapListBlock = 'hydrant-map-list-block';
 if (document.getElementById(hydrantMapListBlock)) {
     new Vue({el: '#' + hydrantMapListBlock, render: h => h(HydrantMapList)});
+}
+
+// 101 карточка
+if (document.getElementById('cardadd101')) {
+    window.add101 = new Add101Functions();
+
+    // @TODO удалить, когда функционал будет реализован
+    document.getElementById('location').addEventListener('keyup', function () {
+        const value = document.getElementById('location').value;
+        if (value.toLowerCase().indexOf('гоголя 133') >= 0) {
+            alert('Школа: Гимназия №15');
+            document.getElementById('storey_count').value = 4;
+            document.getElementById('fire_level_id').value = 3;
+        }
+    });
+
+    window.addEventListener('load', () => {
+        window.add101.bindTimepickers();
+        window.add101.bindAutocomplete();
+        document.getElementById('preload_pane').style.display = 'none';
+        window.add101.setTab(0);
+        document.getElementById('nexttab').addEventListener('click', window.add101.nextTab);
+    });
 }
