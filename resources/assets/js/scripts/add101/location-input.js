604e2db9da212af48edcc65898f6e428ff88b9ff
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2018-08-21 10:27:06 +0600

    Map in 101 card is now syncronized with form by adress change

--- resources/assets/js/scripts/add101/location-input.js
+++ resources/assets/js/scripts/add101/location-input.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import Vue from 'vue';
-import {globalBus} from '../global-bus';
+import { globalBus } from '../global-bus';
 
 const lodash = require('lodash');
 
-export default function bindLocationInputApp() {
+export default function bindLocationInputApp () {
+    const exchangeKey = 'mapscreenLocalStorageExchange';
     const element = document.querySelector('[data-component="location-input"]');
     return new Vue({
         el: element,
@@ -15,6 +16,7 @@ export default function bindLocationInputApp() {
         },
         methods: {
             searchLocationPlans: lodash.debounce(function () {
+                this.notifyMap();
                 axios.get('/ajax/find_special_plan', {
                     params: {
                         location: this.location
@@ -25,36 +27,45 @@ export default function bindLocationInputApp() {
                     this.setData([]);
                 });
             }, 300),
-            setData(items) {
+            setData (items) {
                 this.items = items;
                 this.showList = this.items.length > 0;
-                if (this.items.length === 1 && this.items[0].location === this.location) {
+                if (this.items.length === 1 && this.items[0].location ===
+                    this.location) {
                     this.showList = false;
                 }
             },
-            selectItem(item) {
+            selectItem (item) {
                 this.location = item.location;
                 globalBus.$emit('specialPlanFound', item);
                 this.showList = false;
             },
-            onBlur() {
+            onBlur () {
                 setTimeout(() => {
                     this.showList = false;
                 }, 500);
             },
-            onFocus() {
+            onFocus () {
                 this.setData([]);
                 this.searchLocationPlans();
+            },
+            notifyMap: function () {
+                window.localStorage.setItem(exchangeKey, this.location);
             }
         },
         watch: {
-            location(newValue, oldValue) {
+            location (newValue, oldValue) {
                 if (newValue !== oldValue) {
                     this.searchLocationPlans();
                 }
             }
         },
-        mounted() {
+        mounted () {
+            window.addEventListener('storage', (event) => {
+                if (event.key === exchangeKey) {
+                    this.location = event.newValue;
+                }
+            });
             this.location = element.dataset.value;
         }
     });
