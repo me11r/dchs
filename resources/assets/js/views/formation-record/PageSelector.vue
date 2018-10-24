6bf693ff4c1c160650c29972f418937516255630
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2018-10-24 14:26:17 +0600

    some new templates

--- resources/assets/js/views/formation-record/PageSelector.vue
+++ resources/assets/js/views/formation-record/PageSelector.vue
@@ -4,12 +4,15 @@
             <label for="dept_id_selector">№ ОДС</label>
         </p>
         <div class="select">
-            <select class="select"
-                    name="ods_id"
-                    id="dept_id_selector"
-                    v-model="shiftId_">
-                <option v-for="item in ods_" :value="item.id">
-                &nbsp;{{ item.name }}&nbsp;
+            <select
+                class="select"
+                name="ods_id"
+                id="dept_id_selector"
+                v-model="shiftId_">
+                <option
+                    v-for="item in ods_"
+                    :value="item.id">
+                    &nbsp;{{ item.name }}&nbsp;
                 </option>
             </select>
         </div>
@@ -17,37 +20,37 @@
 </template>
 
 <script>
-    export default {
-        name: "PageSelector",
-        props: {
-            ods: {
-                type: Array,
-                default: () => {}
-            },
-            shiftId: {
-                type: Number,
-                default: 1
-            },
+export default {
+    name: 'PageSelector',
+    props: {
+        ods: {
+            type: Array,
+            default: () => {}
         },
-        data: function () {
-            return {
-                shiftId_: this.shiftId,
-                ods_: this.ods,
-            }
-        },
-        watch: {
-            'shiftId_'(newValue, oldValue) {
-                if (oldValue !== '' && oldValue !== newValue) {
-                    let location = window.location.pathname.split('/');
-                    location.pop();
-                    location.push(newValue);
-                    window.location = location.join('/');
-                }
-            }
-        },
-        methods: {
+        shiftId: {
+            type: Number,
+            default: 1
         }
+    },
+    data: function () {
+        return {
+            shiftId_: this.shiftId,
+            ods_: this.ods
+        };
+    },
+    watch: {
+        'shiftId_'(newValue, oldValue) {
+            if (oldValue !== '' && oldValue !== newValue) {
+                let location = window.location.pathname.split('/');
+                location.pop();
+                location.push(newValue);
+                window.location = location.join('/');
+            }
+        }
+    },
+    methods: {
     }
+};
 </script>
 
 <style scoped>
