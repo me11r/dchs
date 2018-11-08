324d46c01f80b7c4cedae752d31f6056da4cc0db
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2018-11-08 11:24:41 +0600

    Fixing tabs loading on 101 card

--- resources/assets/js/scripts/add101/tabs.js
+++ resources/assets/js/scripts/add101/tabs.js
@@ -1,14 +1,12 @@
 export default class Tabs {
-    // export axios from 'axios';
-
     constructor () {
         this.activeTab = 0;
+        this.tabsCount = 0;
         this.isNewForm = (document.getElementById('card-101-form-id').getAttribute('data-id') === '0');
     }
 
     setTab (i) {
-
-        if(!this.isNext(i)) return false;
+        if (!this.isNext(i)) return false;
 
         const form = document.getElementById('card-101-form');
         let valid = form.checkValidity();
@@ -19,7 +17,7 @@ export default class Tabs {
                 form.action += ('?comeback=' + i);
                 return form.submit();
             } else {
-                if(fire_department_id.value === undefined || fire_department_id.value == false){
+                if (fire_department_id.value === undefined || fire_department_id.value == false) {
                     return false;
                 }
                 form.querySelector('button[type=submit]').click();
@@ -39,14 +37,15 @@ export default class Tabs {
                 form.querySelector('button[type=submit]').click();
             }
         }
-        if(i !== 6) {
+        if (i !== 6) {
             form.querySelector('button[type=submit]').classList.add('is-hidden');
-        }else{
+        } else {
             form.querySelector('button[type=submit]').classList.remove('is-hidden');
         }
 
         let tabs = document.querySelectorAll('#cardadd101 .tabs li');
         let panels = document.querySelectorAll('#cardadd101 .panels > div');
+        this.tabsCount = panels.length;
         tabs[this.activeTab].classList.remove('is-active');
         panels[this.activeTab].classList.add('is-hidden');
         tabs[i].classList.add('is-active');
@@ -56,16 +55,15 @@ export default class Tabs {
 
     nextTab () {
         let nextTab = this.activeTab + 1;
-        if (nextTab > 4) {
+        if (nextTab >= this.tabsCount) {
             nextTab = 0;
         }
         this.setTab(nextTab);
     }
 
-    isNext(step){
-        if(step === 1){
-            if(!document.getElementById('location').value && !document.getElementById('fireplace').value)
-                return false;
+    isNext(step) {
+        if (step === 1) {
+            if (!document.getElementById('location').value && !document.getElementById('fireplace').value) { return false; }
         }
         return true;
     }
