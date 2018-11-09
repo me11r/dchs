cd02f2c8e2c5788485ce0369574f8f917611f01e
alexander1729 <alexander1729@yandex.ru>
2018-11-09 11:35:33 +0600

    # 101 card added chronologies

--- app/Ticket101.php
+++ app/Ticket101.php
@@ -366,16 +366,6 @@ class Ticket101 extends Model
             ->where('department', $service)->first();
     }
 
-    public function on_ways()
-    {
-        return $this->hasMany(OnWay101::class, 'ticket101_id');
-    }
-
-    public function arrived()
-    {
-        return $this->hasMany(Arrived101::class, 'ticket101_id');
-    }
-
     public function wall_material()
     {
         return $this->belongsTo(WallMaterial::class, 'wall_material_id');
