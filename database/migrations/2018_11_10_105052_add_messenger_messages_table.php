9ce57cf09020da5f4ceec785670bd305472ba583
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2018-11-10 11:36:26 +0600

    Added migrations for messenger

---
+++ database/migrations/2018_11_10_105052_add_messenger_messages_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMessengerMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messenger_messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sender_id')->index();
+            $table->integer('reciever_id')->index();
+            $table->enum('message_type', ['text', 'file'])->default('text');
+            $table->integer('file_id')->nullable();
+            $table->text('message')->nullable();
+            $table->boolean('is_viewed')->default(false)->index();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index(['sender_id', 'reciever_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messenger_messages');
+    }
+}
