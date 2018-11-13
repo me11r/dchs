eae1aad3d5da70212dcface4368be62be1eefda0
nbah1990 <uocyxapuk@gmail.com>
2018-11-13 22:06:19 +0600

    - уведомления об измениях в строевых и оперативной инофрмации

--- app/Observers/EmergencySituationObserver.php
+++ app/Observers/EmergencySituationObserver.php
@@ -2,13 +2,21 @@
 
 namespace App\Observers;
 
+use App\Messenger\MessengerTrait;
 use App\Models\EmergencySituation;
 
 class EmergencySituationObserver
 {
+    use MessengerTrait;
+
     public function creating(EmergencySituation $model)
     {
         $model->user_id = \Auth::user()->id;
+        $this->sendMessageAboutFormationAction('Пользователь ' . \Auth::user()->full_username . ' произвёл изменения в оперативной информации');
     }
 
+    public function updating()
+    {
+        $this->sendMessageAboutFormationAction('Пользователь ' . \Auth::user()->full_username . ' произвёл изменения в оперативной информации');
+    }
 }
