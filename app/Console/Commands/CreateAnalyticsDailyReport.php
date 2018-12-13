7c3e544a31f7d5c1ca76f21cdaf5400acb1b02ba
alexander1729 <alexander1729@yandex.ru>
2018-12-13 19:03:47 +0600

    # fixed analytics

--- app/Console/Commands/CreateAnalyticsDailyReport.php
+++ app/Console/Commands/CreateAnalyticsDailyReport.php
@@ -3,6 +3,16 @@
 namespace App\Console\Commands;
 
 use App\Analytics101;
+use App\Dictionary\BurntObject;
+use App\Dictionary\FireObject;
+use App\Reports\Report;
+use App\Repositories\Contracts\BurntObjectInterface;
+use App\Repositories\Contracts\FireObjectInterface;
+use App\Repositories\Contracts\Ticket101Interface;
+use App\Repositories\EloquentBurntObjectRepository;
+use App\Repositories\EloquentFireObjectRepository;
+use App\Repositories\EloquentTicket101Repository;
+use App\Ticket101;
 use Illuminate\Console\Command;
 
 class CreateAnalyticsDailyReport extends Command
@@ -38,8 +48,25 @@ class CreateAnalyticsDailyReport extends Command
      */
     public function handle()
     {
-        $report = Analytics101::create([
+        $data['record'] = Analytics101::firstOrCreate([
             'date' => today(),
         ]);
+
+        $report = (new Report(new EloquentTicket101Repository(), new EloquentFireObjectRepository(), new EloquentBurntObjectRepository()))->getReport();
+
+
+        if(isset($report['tripResults']) && count($report['tripResults'])){
+            foreach ($report['tripResults'] as $title => $items) {
+                foreach ($items as $reportItem) {
+                    $data['record']->items()->firstOrCreate(
+                        ['ticket101_id' => $reportItem['id']],
+                        [
+                            'text' => $reportItem['analytics'],
+                            'trip_result_id' => $reportItem['trip_result_id'],
+                            'ticket101_id' => $reportItem['id'],
+                        ]);
+                }
+            }
+        }
     }
 }
