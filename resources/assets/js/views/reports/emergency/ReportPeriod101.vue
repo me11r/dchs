b3c41888b5e9c6910dd684aa06de6946c46b6921
Rai Maemirov <rai.maemirov@yandex.ru>
2018-12-18 10:15:56 +0600

    resolve ARM-283 amp hot ARM-285

--- resources/assets/js/views/reports/emergency/ReportPeriod101.vue
+++ resources/assets/js/views/reports/emergency/ReportPeriod101.vue
@@ -14,17 +14,18 @@
                             @click.prevent="print()"><i class="fas fa-print"></i>&nbsp;Печать</button>
                     </div>
                     <div class="level-right has-text-right">
-                        <a
-                                href="/xls/report101/emergency"
-                                class="button is-primary"
-                                ><i class="fas fa-print"></i>&nbsp;Сохранить в XLS
-                        </a>
+                        <button
+                            @click.prevent="exportXls"
+                            class="button is-primary"
+                        >
+                            <i class="fas fa-print"></i>&nbsp;Сохранить в XLS
+                        </button>
                     </div>
                 </div>
                 <br>
                 <form>
                     <div class="field">
-                        <label for="reason">Результат выезда</label>
+                        <label for="reason">Результат выезда:</label>
                         <select
                             @change="selectResult"
                             class="select"
@@ -39,20 +40,52 @@
                         </select>
                     </div>
                     <div class="field">
-                        <label for="">Начало периода</label>
+                        <label for="burnt">Объект горения:</label>
+                        <select
+                            @change="post_data"
+                            class="select"
+                            v-model="burnt_id"
+                            id="burnt">
+                            <option value=""></option>
+                            <option
+                                v-for="item in burnt_objects"
+                                :value="item.id"
+                                :key="item.id">{{ item.name }}
+                            </option>
+                        </select>
+                    </div>
+                    <div class="field">
+                        <label for="cityArea">Район города:</label>
+                        <select
+                            @change="post_data"
+                            class="select"
+                            v-model="city_area_id"
+                            id="cityArea">
+                            <option value=""></option>
+                            <option
+                                v-for="item in city_areas"
+                                :value="item.id"
+                                :key="item.id">{{ item.name }}
+                            </option>
+                        </select>
+                    </div>
+                    <div class="field">
+                        <label for="startPeriod">Начало периода:</label>
                         <input
                             @blur="selectPeriod"
                             v-model="date_begin_"
                             type="date"
-                            class="date">
+                            class="date"
+                            id="startPeriod">
                     </div>
                     <div class="field">
-                        <label for="">Конец периода</label>
+                        <label for="endPeriod">Конец периода:</label>
                         <input
                             @blur="selectPeriod"
                             v-model="date_end_"
                             class="date"
-                            type="date">
+                            type="date"
+                            id="endPeriod">
                     </div>
                     <table class="formation-record-table">
                         <thead>
@@ -80,37 +113,43 @@
                             </tr>
                         </thead>
                         <tbody>
-                        <tr v-for="item in report_summary">
-                            <td>{{ item.trip_result ? item.trip_result.name : '' }}</td>
-                            <td>{{ item.burn_object ? item.burn_object.name : '' }}</td>
-                            <td>{{ item.city_area ? item.city_area.name : '' }}</td>
-                            <td>{{ item.location }}</td>
-                            <td>{{ item.rescued_count }}</td>
-                            <td>{{ item.evac_count }}</td>
-                            <td>{{ item.co2_poisoned_count }}</td>
-                            <td>{{ item.ch4_poisoned_count }}</td>
-                            <td>{{ item.gpt_burns_count }}</td>
-                            <td>{{ item.people_death_count }}</td>
-                            <td>{{ item.children_death_count }}</td>
-                            <td>{{ item.hospitalized_count }}</td>
+                            <tr
+                                v-for="(item, idx) in report_summary"
+                                :key="`rpt_smm_${idx}`">
+                                <td>{{ item.trip_result ? item.trip_result.name : '' }}</td>
+                                <td>{{ item.burn_object ? item.burn_object.name : '' }}</td>
+                                <td>{{ item.city_area ? item.city_area.name : '' }}</td>
+                                <td>{{ item.location }}</td>
+                                <td>{{ item.rescued_count }}</td>
+                                <td>{{ item.evac_count }}</td>
+                                <td>{{ item.co2_poisoned_count }}</td>
+                                <td>{{ item.ch4_poisoned_count }}</td>
+                                <td>{{ item.gpt_burns_count }}</td>
+                                <td>{{ item.people_death_count }}</td>
+                                <td>{{ item.children_death_count }}</td>
+                                <td>{{ item.hospitalized_count }}</td>
 
-                            <td>{{ item.first_arrived_time }}</td>
-                            <td>{{ item.on_way_time }}</td> <!--из вкладки "Высылка" по формуле "время прибытия" - "время выезда"-->
-                            <td>{{ item.liqv_time }}</td>
-                            <td>{{ item.loc_time }}</td>
-                            <td>
-                                <p v-for="chronology in item.chronology">
-                                    Количество: {{ chronology.quantity }}<br>
-                                </p>
-                            </td>
-                            <td>
-                                <p v-for="chronology in item.chronology">
-                                    Количество: {{ chronology.working_time }}<br>
-                                </p>
-                            </td>
-                            <td>{{ item.loc_time_total }}</td> <!--по формуле "время локализации" - "время прибытия" -->
-                            <td>{{ item.liqv_time_total }}</td> <!--по формуле "время ликвидации" - "время прибытия" -->
-                        </tr>
+                                <td>{{ item.first_arrived_time }}</td>
+                                <td>{{ item.on_way_time }}</td> <!--из вкладки "Высылка" по формуле "время прибытия" - "время выезда"-->
+                                <td>{{ item.liqv_time }}</td>
+                                <td>{{ item.loc_time }}</td>
+                                <td>
+                                    <p
+                                        v-for="(chronology, idx) in item.chronology"
+                                        :key="`chr_qnt_${idx}`">
+                                        Количество: {{ chronology.quantity }}<br>
+                                    </p>
+                                </td>
+                                <td>
+                                    <p
+                                        v-for="(chronology, idx) in item.chronology"
+                                        :key="`chr_wrkt_${idx}`">
+                                        Количество: {{ chronology.working_time }}<br>
+                                    </p>
+                                </td>
+                                <td>{{ item.loc_time_total }}</td> <!--по формуле "время локализации" - "время прибытия" -->
+                                <td>{{ item.liqv_time_total }}</td> <!--по формуле "время ликвидации" - "время прибытия" -->
+                            </tr>
                         </tbody>
                     </table>
                     <!--<div class="buttons has-text-right is-grouped is-right" style="">-->
@@ -124,6 +163,8 @@
 
 <script>
 import axios from 'axios';
+import { serialize } from '../../../utils';
+
 export default {
     name: 'ReportPeriod101',
     props: {
@@ -138,16 +179,26 @@ export default {
         results: {
             type: Array,
             default: () => {}
+        },
+        burnt_objects: {
+            type: Array,
+            default: () => []
+        },
+        city_areas: {
+            type: Array,
+            default: () => []
         }
     },
-    data: function () {
+    data() {
         return {
             csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
             date_begin_: this.date_begin,
             date_end_: this.date_end,
             report_summary: {},
             result_id: null,
-            results_: this.results
+            results_: this.results,
+            burnt_id: null,
+            city_area_id: null
         };
     },
     methods: {
@@ -168,7 +219,9 @@ export default {
             axios.post('/reports/101/emergency', {
                 date_begin: self.date_begin_,
                 date_end: self.date_end_,
-                result_id: self.result_id
+                result_id: self.result_id,
+                burnt_id: self.burnt_id,
+                city_area_id: self.city_area_id
             }).then((resp) => {
                 self.report_summary = resp.data;
 
@@ -178,6 +231,19 @@ export default {
 
         selectPeriod() {
             this.post_data();
+        },
+
+        exportXls() {
+            let self = this;
+            let queries = {
+                date_begin: self.date_begin_,
+                date_end: self.date_end_,
+                result_id: self.result_id,
+                burnt_id: self.burnt_id,
+                city_area_id: self.city_area_id
+            };
+
+            window.location.href = `/xls/report101/emergency?${serialize(queries)}`;
         }
     },
 
