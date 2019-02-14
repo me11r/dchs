25131cbd6ffb8b85b12d85050e2d59d6a4029acd
alexander1729 <alexander1729@yandex.ru>
2019-02-14 13:45:20 +0600

    # added emergency_situation to report 112

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -10,4 +10,37 @@ abstract class BaseModel extends Model
     {
         return $search ? $q->where($field, $search) : $q;
     }
+
+    public function scopeShiftRecords($q, $hoursBegin = 7, $hoursEnd = 7)
+    {
+        $nowHours = now()->hour;
+
+        //если текущее время от 00:00 до 07:00, берем интервал с 07:00 прошлого дня по 07:00 текущего
+
+        //если текущее время от 07:00, интервал = 07:00 текущего дня по 07:00 следующего дня
+        if($nowHours < 7) {
+
+            $finalDateBegin = today()
+                ->addDay(-1)
+                ->addHours($hoursBegin)
+                ->format('Y-m-d H:i:s');
+
+            $finalDateEnd = today()
+                ->addHours($hoursEnd)
+                ->format('Y-m-d H:i:s');
+        }
+        else {
+
+            $finalDateBegin = today()
+                ->addHours($hoursBegin)
+                ->format('Y-m-d H:i:s');
+
+            $finalDateEnd = today()
+                ->addDay(1)
+                ->addHours($hoursEnd)
+                ->format('Y-m-d H:i:s');
+        }
+
+        return $q->whereBetween('created_at', [$finalDateBegin, $finalDateEnd]);
+    }
 }
