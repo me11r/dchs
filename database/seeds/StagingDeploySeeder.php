5d7c0fa2099f0ed9b6f41f741c505ffa3e424d7f
alexander1729 <alexander1729@yandex.ru>
2019-03-07 18:51:39 +0600

    #copy wav file

--- database/seeds/StagingDeploySeeder.php
+++ database/seeds/StagingDeploySeeder.php
@@ -7,6 +7,11 @@ class StagingDeploySeeder extends Seeder
     public function run()
     {
         //$this->call(ChunkedBuildingsSeeder::class);
+        $copyFrom = public_path('alarm.wav');
+        $copyTo = public_path('assets/alarm.wav');
+        if(file_exists($copyFrom) && !file_exists($copyTo)) {
+            copy($copyFrom, $copyTo);
+        }
 
         \App\FireDepartment::firstOrCreate([
             'title' => 'ОД',
