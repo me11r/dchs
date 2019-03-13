5526eeec83b9ea3253f4d54d0b0665c8ed102c6d
alexander1729 <alexander1729@yandex.ru>
2019-03-13 10:28:18 +0600

    # changed notifications

--- app/Observers/FormationReportObserver.php
+++ app/Observers/FormationReportObserver.php
@@ -2,23 +2,31 @@
 
 namespace App\Observers;
 
+use App\FormationReport;
 use App\Messenger\MessengerTrait;
 
 class FormationReportObserver
 {
     use MessengerTrait;
 
-    public function creating()
+    public function created(FormationReport $report)
     {
         if (\Auth::user()){
-            $this->sendMessageAboutFormationAction('Пользователь ' . \Auth::user()->full_username . ' произвёл изменения в строевой записке');
+            $url = $this->generateUrl($report);
+            $this->sendMessageAboutFormationAction('Пользователь ' . \Auth::user()->full_username . ' произвёл изменения в строевой записке',$url);
         }
     }
 
-    public function updating()
+    public function updating(FormationReport $report)
     {
         if (\Auth::user()) {
-            $this->sendMessageAboutFormationAction('Пользователь ' . \Auth::user()->full_username . ' произвёл изменения в строевой записке');
+            $url = $this->generateUrl($report);
+            $this->sendMessageAboutFormationAction('Пользователь ' . \Auth::user()->full_username . ' произвёл изменения в строевой записке',$url);
         }
+    }
+
+    private function generateUrl($model)
+    {
+        return "/formation-record";
     }
 }
