125c567bf49506fd513cbadde2d88d1e2a062f5f
alexander1729@yandex.ru <3afojuv6410>
2019-04-01 17:46:56 +0600

    # added `note` field to alert system items

--- app/AlertSystemCheckItem.php
+++ app/AlertSystemCheckItem.php
@@ -10,6 +10,7 @@ class AlertSystemCheckItem extends Model
         'check1',
         'check2',
         'check3',
+        'note',
         'direction_id',
         'alert_system_check_id',
     ];
@@ -19,9 +20,37 @@ class AlertSystemCheckItem extends Model
         return $this->belongsTo(Direction::class, 'direction_id');
     }
 
+    public function getTitle($val)
+    {
+        if ($val === 1) {
+            return 'Исправен';
+        }
+
+        if ($val === 0) {
+            return 'Не исправен';
+        }
+
+        return null;
+    }
+
     public function alert_system_check()
     {
         return $this->belongsTo(AlertSystemCheck::class, 'alert_system_check_id');
+    }
+
+    public function getCheck1TitleAttribute()
+    {
+        return $this->getTitle($this->check1);
+    }
+
+    public function getCheck2TitleAttribute()
+    {
+        return $this->getTitle($this->check2);
+    }
+
+    public function getCheck3TitleAttribute()
+    {
+        return $this->getTitle($this->check3);
     }
 
 
