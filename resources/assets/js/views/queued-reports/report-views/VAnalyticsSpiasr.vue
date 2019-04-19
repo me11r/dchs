7e7b528a94eb5b3e04eb9c21a084320f4e3f9c99
nbah1990 <ivan.karatayev@gmail.com>
2019-04-19 14:20:47 +0600

    - queued reports raw query

--- resources/assets/js/views/queued-reports/report-views/VAnalyticsSpiasr.vue
+++ resources/assets/js/views/queued-reports/report-views/VAnalyticsSpiasr.vue
@@ -2,8 +2,75 @@
     <div class="panel">
         <div
             class="field">
-            <h3 class="title">Общее количество выездов: {{ summaryFiltered.length }}</h3>
+            <h4 class="title">Общее количество выездов: {{ report_summary['items'].length }}</h4>
         </div>
+        <hr>
+        <div class="field">
+            <h4 class="title">Время следования</h4>
+
+            <table class="formation-record-table">
+                <thead>
+                    <tr>
+                        <th>до 5 минут</th>
+                        <th>до 10 минут</th>
+                        <th>более 10 минут</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{{ report_summary['totals']['on_way_totals']['less_5'] }}</td>
+                        <td>{{ report_summary['totals']['on_way_totals']['less_10'] }}</td>
+                        <td>{{ report_summary['totals']['on_way_totals']['more_10'] }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="field">
+            <h4 class="title">Время ликвидации</h4>
+
+            <table class="formation-record-table">
+                <thead>
+                    <tr>
+                        <th>до 15 минут</th>
+                        <th>до 30 минут</th>
+                        <th>до 1 часа</th>
+                        <th>до 2 часов</th>
+                        <th>более 2 часов</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{{ report_summary['totals']['liqv_totals']['less_15'] }}</td>
+                        <td>{{ report_summary['totals']['liqv_totals']['less_30'] }}</td>
+                        <td>{{ report_summary['totals']['liqv_totals']['less_60'] }}</td>
+                        <td>{{ report_summary['totals']['liqv_totals']['less_120'] }}</td>
+                        <td>{{ report_summary['totals']['liqv_totals']['more_120'] }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="field">
+            <h4 class="title">Звенья ГДЗС</h4>
+
+            <table class="formation-record-table">
+                <thead>
+                    <tr>
+                        <th>одним звеном</th>
+                        <th>двумя и более</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{{ report_summary['totals']['elements_of_gdzs']['one'] }}</td>
+                        <td>{{ report_summary['totals']['elements_of_gdzs']['many'] }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <hr>
         <div
             class="tbl"
             style="overflow-x: scroll;">
@@ -38,57 +105,31 @@
                 </thead>
                 <tbody>
                     <tr
-                        v-for="(item, idx) in summaryFiltered"
+                        v-for="(item, idx) in report_summary['items']"
                         :key="`rpt_smm_${idx}`">
 
-                        <td>{{ item.custom_created_at|dateFilter('DD.MM.YYYY') }}</td>
-                        <td>{{ item.custom_created_at|dateFilter('HH:mm') }}</td>
+                        <td>{{ item.custom_created_at_date }}</td>
+                        <td>{{ item.custom_created_at_hours }}</td>
                         <td>{{ item.caller_name }}</td>
                         <td>{{ item.caller_phone }}</td>
-                        <td>{{ item.city_area ? item.city_area.name : '' }}</td>
+                        <td>{{ item.city_area_name }}</td>
                         <td>{{ item.location }}</td>
                         <td>{{ item.object_name }}</td>
-                        <td>{{ item.result_fire_level ? item.result_fire_level.name : '' }}</td> <!--УЧАСТНИКИ ТУШЕНИЯ-->
-                        <td>{{ item.liquidation_method ? item.liquidation_method.name : '' }}</td> <!--Ликвидировано стволами-->
+                        <td>{{ item.result_fire_level_name }}</td> <!--УЧАСТНИКИ ТУШЕНИЯ-->
+                        <td>{{ item.liquidation_method_name }}</td> <!--Ликвидировано стволами-->
                         <td>{{ item.on_way_time }}</td>
                         <td>{{ item.loc_time }}</td>
                         <td>{{ item.liqv_time }}</td>
                         <td>{{ item.loc_time_total }}</td>
-<!--                        <td>-->
-<!--                            <p-->
-<!--                                v-for="(chronology, idx) in item.chronologies"-->
-<!--                                v-if="chronology.event_info_arrived_id !== null && chronology.event_info_arrived.name !== 'ГДЗС'"-->
-<!--                                :key="`chr_qnt_${idx}`">-->
-<!--                                Тип {{ chronology.event_info_arrived.name }}-->
-<!--                                Количество: {{ chronology.quantity }}<br>-->
-<!--                            </p>-->
-<!--                        </td> &lt;!&ndash;СТВОЛЫ&ndash;&gt;-->
                         <td>{{ item.gdzs_count }}</td> <!--Звенья ГДЗС-->
-<!--                        <td>-->
-<!--                            <p-->
-<!--                                v-for="(chronology, idx) in item.chronologies"-->
-<!--                                v-if="chronology.event_info_arrived_id !== null && chronology.event_info_arrived.name !== 'ГДЗС'"-->
-<!--                                :key="`chr_woring_time_${idx}`">-->
-<!--                                Тип {{ chronology.event_info_arrived.name }}-->
-<!--                                Количество: {{ chronology.working_time }}<br>-->
-<!--                            </p>-->
-<!--                        </td> &lt;!&ndash;Время работы стволов&ndash;&gt;-->
-                        <td>
-                            <p
-                                v-for="(chronology, idx) in item.chronologies"
-                                v-if="chronology.event_info_arrived_id !== null && chronology.event_info_arrived.name === 'ГДЗС'"
-                                :key="`chr_woring_time_gdzs_${idx}`">
-                                Тип {{ chronology.event_info_arrived.name }}
-                                Количество: {{ chronology.working_time }}<br>
-                            </p>
-                        </td> <!--Время работы ГДЗС-->
+                        <td>{{ item.event_info_arrived_names }}</td> <!--Время работы ГДЗС-->
                         <td>{{ item.rescued_count }}</td>
                         <td>{{ item.evac_count }}</td>
                         <td>{{ item.gpt_burns_count }}</td>
-                        <td>{{ item.people_death_count + item.children_death_count }}</td>
+                        <td>{{ item.total_death_count }}</td>
                         <td>{{ item.loc_time_total }}</td> <!--Затраченное время на локализацию--> <!--время локализации - время прибытия-->
                         <td>{{ item.liqv_time_total }}</td> <!--Затраченное время на ликвидацию--> <!--время ликвидации - время прибытия-->
-                        <td>{{ item.trip_result ? item.trip_result.name : '' }}</td>
+                        <td>{{ item.trip_result_name }}</td>
                         <td>{{ item.max_square }}</td>
                         <td>{{ item.storey_count }}</td>
                     </tr>
@@ -107,7 +148,14 @@ export default {
     mixins: [ReportViewMixin],
     data() {
         return {
-            report_summary: {}
+            report_summary: {
+                items: [],
+                totals: {
+                    on_way_totals: {},
+                    liqv_totals: {},
+                    elements_of_gdzs: {}
+                }
+            }
         };
     },
     computed: {
