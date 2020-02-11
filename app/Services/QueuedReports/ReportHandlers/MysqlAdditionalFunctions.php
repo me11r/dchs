1aad202a182f6df9f455e67ad83c2f7798fc1bd0
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2020-02-11 15:38:02 +0600

    time spike fixing

--- app/Services/QueuedReports/ReportHandlers/MysqlAdditionalFunctions.php
+++ app/Services/QueuedReports/ReportHandlers/MysqlAdditionalFunctions.php
@@ -4,30 +4,39 @@
 namespace App\Services\QueuedReports\ReportHandlers;
 
 
-use Illuminate\Support\Facades\DB;
 //
 trait MysqlAdditionalFunctions
 {
-    protected function defineTimeDiffSpike($outputFormat = '%Hч. %iм.'): void
+    protected function defineTimeDiffSpike(): void
     {
-        DB::unprepared('DROP FUNCTION IF EXISTS TIME_DIFF_SPIKE');
-        $function = DB::raw("CREATE FUNCTION TIME_DIFF_SPIKE(first_time TIME, second_time TIME) RETURNS VARCHAR(20) CHARSET utf8
-                      LANGUAGE SQL
-                    BEGIN
-                      DECLARE first_date TIMESTAMP;
-                      DECLARE second_date TIMESTAMP;
-                      DECLARE output_format VARCHAR(50) CHARSET utf8 DEFAULT '$outputFormat';
-                    
-                      SET first_date = TIMESTAMP('2000-01-01', first_time);
-                    
-                      IF second_time < first_time THEN
-                        SET second_date = TIMESTAMP('2000-01-02', second_time);
-                      ELSE
-                        SET second_date = TIMESTAMP('2000-01-01', second_time);
-                      END IF;
-                    
-                      RETURN TIME_FORMAT(ABS(TIMEDIFF(first_date, second_date)), output_format);
-                    END");
-        DB::unprepared($function);
+        return;
+        /**
+         * @deprecated
+         * @see moved to database migration!
+         * DB::unprepared('DROP FUNCTION IF EXISTS TIME_DIFF_SPIKE');
+         * $function = DB::raw(
+         * "CREATE FUNCTION TIME_DIFF_SPIKE(first_time TIME, second_time TIME)
+         * RETURNS VARCHAR(20)
+         * CHARSET utf8
+         * LANGUAGE SQL
+         * DETERMINISTIC
+         * BEGIN
+         * DECLARE first_date TIMESTAMP;
+         * DECLARE second_date TIMESTAMP;
+         * DECLARE output_format VARCHAR(50) CHARSET utf8 DEFAULT '$outputFormat';
+         *
+         * SET first_date = TIMESTAMP('2000-01-01', first_time);
+         *
+         * IF second_time < first_time THEN
+         * SET second_date = TIMESTAMP('2000-01-02', second_time);
+         * ELSE
+         * SET second_date = TIMESTAMP('2000-01-01', second_time);
+         * END IF;
+         *
+         * RETURN TIME_FORMAT(ABS(TIMEDIFF(first_date, second_date)), output_format);
+         * END"
+         * );
+         * DB::unprepared($function);
+         */
     }
 }
---
+++ database/migrations/2020_02_11_153314_move_time_diff_spike_to_migration.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class MoveTimeDiffSpikeToMigration extends Migration
+{
+    private static $SQL = "CREATE FUNCTION TIME_DIFF_SPIKE(first_time TIME, second_time TIME)
+    RETURNS VARCHAR(20) CHARSET utf8
+    LANGUAGE SQL
+    NO SQL 
+    DETERMINISTIC
+BEGIN
+    DECLARE first_date TIMESTAMP;
+    DECLARE second_date TIMESTAMP;
+    DECLARE output_format VARCHAR(50) CHARSET utf8 DEFAULT '%Hч. %iм.';
+
+    SET first_date = TIMESTAMP('2000-01-01', first_time);
+
+    IF second_time < first_time THEN
+        SET second_date = TIMESTAMP('2000-01-02', second_time);
+    ELSE
+        SET second_date = TIMESTAMP('2000-01-01', second_time);
+    END IF;
+
+    RETURN TIME_FORMAT(ABS(TIMEDIFF(first_date, second_date)), output_format);
+END";
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared(self::$SQL);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP FUNCTION IF EXISTS TIME_DIFF_SPIKE');
+    }
+}
